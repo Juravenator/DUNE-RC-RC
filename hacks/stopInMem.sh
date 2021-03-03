b4ae835bcf7600a3ce028d7501bf446dec98f61e
Glenn Dirkx <glenn.dirkx@cern.ch>
2021-03-03 18:57:48 +0100

    make sure all daq apps are killed on cleanup

--- hacks/stopInMem.sh
+++ hacks/stopInMem.sh
@@ -3,6 +3,9 @@ set -o errexit -o nounset -o pipefail
 IFS=$'\n\t\v'
 cd `dirname "${BASH_SOURCE[0]:-$0}"`
 
+# kill all nomad jobs
 ! /tmp/dune-rc-lxplus/bin/nomad status | awk 'NR>1{print $1}' | xargs -iID /tmp/dune-rc-lxplus/bin/nomad stop ID
 ! pkill nomad
 ! pkill consul
+# in case you bricked nomad
+! killall daq_application
